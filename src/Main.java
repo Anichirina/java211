d8a0f305735d2c4d76f9f8ded5cf0bc415195178
Irina Anichkova <anichkova2016@yandex.ru>
2020-11-09 18:02:23 +0300

    New file

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main (String[] args) {
+        int ticket = 4023;
+        int part_ticket = 20;
+        int total_miles = (int) ticket / (int)part_ticket;
+        System.out.println(total_miles);
+    }
+}
