19b21c3974d7f85ab05edf505f82a23ab60ed27f
Irina Anichkova <anichkova2016@yandex.ru>
2020-11-21 20:06:58 +0300

    Modified: src

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main (String[] args) {
         int ticket = 4023;
         int part_ticket = 20;
-        int total_miles = (int) ticket / (int)part_ticket;
+        int total_miles = ticket / part_ticket;
         System.out.println(total_miles);
     }
 }
